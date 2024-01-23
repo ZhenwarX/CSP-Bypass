07f2b32d9c2b60481bbe3d1ad4805a64f5737cfc
ZhenwarX <61203950+ZhenwarX@users.noreply.github.com>
2024-01-23 16:39:04 +0000

    Update t.js

--- t.js
+++ t.js
@@ -1,16 +1,14 @@
-const url = 'https://dev1.dev.clover.com/v3/accounts/current?expand=primaryDeveloper%2CprimaryEnterprise%2CprimaryMerchant%2CprimaryMerchant.properties%2CprimaryReseller&_=1706027175630';
+// t.js
 
-// Create a headers object with credentials set to 'include'
-const headers = new Headers({
-  'Content-Type': 'application/json',
-});
+function fetchData() {
+  const url = 'https://dev1.dev.clover.com/v3/accounts/current?expand=primaryDeveloper%2CprimaryEnterprise%2CprimaryMerchant%2CprimaryMerchant.properties%2CprimaryReseller&_=1706027175630';
+  const headers = new Headers({ 'Content-Type': 'application/json' });
 
-// Use the fetch API to make the request
-fetch(url, {
-  method: 'GET',
-  headers: headers,
-  credentials: 'include', // This includes credentials (like cookies) in the request
-})
+  fetch(url, { 
+    method: 'GET', 
+    headers: headers, 
+    credentials: 'include' 
+  })
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -18,10 +16,12 @@ fetch(url, {
     return response.json();
   })
   .then(data => {
-    // Handle the response data here
     console.log(data);
   })
   .catch(error => {
-    // Handle errors here
     console.error('Fetch error:', error);
   });
+}
+
+// Call the fetchData function when the script is loaded
+fetchData();
