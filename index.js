937a5984ed10888829e3b6f4af4cee72ba45a27d
ZhenwarX <61203950+ZhenwarX@users.noreply.github.com>
2024-01-23 13:08:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/* =================================== !/
-
-X55 is... Cross-Site Scripting!
-
-Please check below on how to use this domain for XSS:
-https://twitter.com/brutelogic/status/1488523712244637708
-
-By @brutelogic
-Visit https://brutelogic.com.br for more XSS stuff.
-
-/! =================================== */
-
 if (document.location.hash) {
    var frag = document.location.hash.match(/[^#][^#]*/)[0];
 }
@@ -28,6 +16,6 @@ if (frag) {
 
 } else {
 
-  alert(1);
+  alert(document.domain);
 
 }
